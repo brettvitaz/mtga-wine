9c5198c559fd30c82bcadf7b3e5a5f850c5374d9
brettvitaz <bvitaz@cardkingdom.com>
2019-07-31 00:03:26 -0700

    export wine env vars

--- bin/build.sh
+++ bin/build.sh
@@ -5,13 +5,16 @@ TMP_DIR="${PROJECT_DIR}/tmp"
 OUT_DIR="${PROJECT_DIR}/out"
 SKEL_DIR="${PROJECT_DIR}/wrapper_skel"
 
+WINE_DIR="/Applications/Wine Stable.app/Contents/Resources/wine/bin"
+export PATH="${WINE_DIR}:$PATH"
+
+WINEARCH=win32 
+WINEPREFIX="${TMP_DIR}/bottle"
+
 mkdir -p "${TMP_DIR}"
 cd "${TMP_DIR}"
 
 wget -c https://mtgarena.downloads.wizards.com/Live/Windows32/versions/1595.718832/MTGAInstaller_0.1.1595.718832.msi
-
-WINEARCH=win32 
-WINEPREFIX="${TMP_DIR}/bottle"
 
 echo "Installing Dot Net 4.6.2"
 winetricks -q dotnet462 &> /dev/null
